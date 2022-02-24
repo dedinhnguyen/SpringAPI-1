9e013da18d8190e97f07c45c43d100b2e17ce4d0
dedinhnguyen <77568163+dedinhnguyen@users.noreply.github.com>
2022-02-24 16:39:51 +0700

    Update SinhvienController.java

--- src/main/java/com/example/demo/controller/SinhvienController.java
+++ src/main/java/com/example/demo/controller/SinhvienController.java
@@ -6,8 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,7 +46,16 @@ public class SinhvienController {
 	@DeleteMapping("{id}")
 	public ResponseEntity<String> DeleteSinhvienById(@PathVariable("id") long Sinhvienid){
 		sinhvienservice.DeleteSinhvienById(Sinhvienid);
-		return new ResponseEntity<String>("Xoa thanh cong! ", HttpStatus.OK);
+		return new ResponseEntity<String>("Xoa thanh cong! ", HttpStatus.NO_CONTENT);
 	}
-
+	@PutMapping("{id}")
+	public ResponseEntity<Sinhvien> updateSinhVienById(@PathVariable("id") long sinhvienid, @RequestBody Sinhvien sinhvien){
+		return new ResponseEntity<Sinhvien>(sinhvienservice.updateSinhvien(sinhvien,sinhvienid), HttpStatus.OK);
+	}
+	
+	@PatchMapping("{id}")
+	public ResponseEntity<Sinhvien> updateSinhVien(@PathVariable("id") long sinhvienid, @RequestBody Sinhvien sinhvien){
+		return new ResponseEntity<Sinhvien>(sinhvienservice.updateSinhvien(sinhvien,sinhvienid), HttpStatus.OK);
+	}
 }
+
