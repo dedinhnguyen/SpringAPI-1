d7a1845c17d872cfd905fdc0b3661bcb743ab359
dedinhnguyen <77568163+dedinhnguyen@users.noreply.github.com>
2022-07-21 00:20:50 +0700

    haha
    21/7

--- src/main/java/com/example/demo/repository/LophocRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Lophoc;
-
-public interface LophocRepository extends JpaRepository<Lophoc, Long>{
-
-}
